53724875f58c0dfc7f0a98a1100f690f4f4a868a
quyetnv <quyetnv.mlhn@gmail.com>
2024-06-11 14:28:53 +0700

    update missed file

---
+++ lib/data/data_sources/board_game_api/local/local_board_game_api.dart
@@ -0,0 +1,34 @@
+import 'package:calculate_card_score/data/data_sources/board_game_api/board_game_api.dart';
+import 'package:calculate_card_score/data/models/score_board_model.dart';
+import 'package:hive/hive.dart';
+
+class LocalBoardGameApi implements BoardGameApi {
+  static const String boardGameBoxName = 'boardGameBox';
+  static const String roundBoxName = 'roundBox';
+
+  late Box<ScoreBoard> boardGameBox;
+
+  Future<void> initialize() async {
+    boardGameBox = await Hive.openBox(boardGameBoxName);
+  }
+
+  @override
+  Future<int> addGame(ScoreBoard scoreBoard) async {
+    return boardGameBox.add(scoreBoard);
+  }
+
+  @override
+  Future<void> deleteGame(int gameId) async {
+    await boardGameBox.delete(gameId);
+  }
+
+  @override
+  Future<void> updateGame(int gameId, ScoreBoard scoreBoard) async {
+    await boardGameBox.put(gameId, scoreBoard);
+  }
+
+  @override
+  Future<List<dynamic>> getAllGames() async {
+    return boardGameBox.values.toList();
+  }
+}
